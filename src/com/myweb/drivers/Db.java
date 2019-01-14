05f661f91a681af085911785b07f83f59eb58627
lxy235 <631990010@qq.com>
2019-01-14 19:40:09 +0800

    fix db bug

--- src/com/myweb/drivers/Db.java
+++ src/com/myweb/drivers/Db.java
@@ -1,10 +1,8 @@
 package com.myweb.drivers;
 
+import com.myweb.drivers.mysql.Mysql;
 import com.myweb.utils.Config;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Db {
     Config config = null;
@@ -16,44 +14,9 @@ public class Db {
         String driver = config.getAttribute("driver");
         switch (driver) {
             case "mysql":
-                try {
-                    String driverclass = config.getAttribute("driverclass");
-                    Class.forName(driverclass);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if(null==conn) {
-                        String user = config.getAttribute("user");
-                        String passwd = config.getAttribute("passwd");
-                        String host = config.getAttribute("host");
-                        String port = config.getAttribute("port");
-                        String db = config.getAttribute("db");
-                        String dsn = "jdbc:mysql://"+host+":"+port+"/"+db+"";
-                        Connection conn = DriverManager.getConnection(dsn,user, passwd);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+                Mysql mysql = new Mysql();
+                conn = mysql.getConnection();
         }
         return conn;
-    }
-    public void Query(String tableName, String sql) {
-
-    }
-    public void Update(String tableName, String fields, String values) {
-
-    }
-    public void Delete(String tableName, String id) {
-
-    }
-    public void execute(String tableName, String sql) {
-
-    }
-    public void commit(String tableName) {
-
-    }
-    public void rollback(String tableName) {
-
     }
 }
--- src/com/myweb/drivers/mysql/Mysql.java
+++ src/com/myweb/drivers/mysql/Mysql.java
@@ -1,5 +1,55 @@
 package com.myweb.drivers.mysql;
 
-public class Mysql {
+import com.myweb.utils.Config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Mysql {
+    Config config = null;
+    Connection conn = null;
+    public Mysql() {
+        config = new Config("db.properties");
+    }
+    public Connection getConnection() {
+        try {
+            String driverclass = config.getAttribute("driverclass");
+            Class.forName(driverclass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            if(null==conn) {
+                String user = config.getAttribute("user");
+                String passwd = config.getAttribute("passwd");
+                String host = config.getAttribute("host");
+                String port = config.getAttribute("port");
+                String db = config.getAttribute("db");
+                String dsn = "jdbc:mysql://"+host+":"+port+"/"+db+"";
+                Connection conn = DriverManager.getConnection(dsn,user, passwd);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    public void Query(String tableName, String sql) {
+
+    }
+    public void Update(String tableName, String fields, String values) {
+
+    }
+    public void Delete(String tableName, String id) {
+
+    }
+    public void execute(String tableName, String sql) {
+
+    }
+    public void commit(String tableName) {
+
+    }
+    public void rollback(String tableName) {
+
+    }
 }
