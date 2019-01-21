8b22a5271ac3544e0c753f22e8b22eb2736a2ec3
lxy235 <631990010@qq.com>
2019-01-21 14:23:01 +0800

    优化过滤器

--- src/com/myweb/servlets/filters/MyWebFilter.java
+++ src/com/myweb/servlets/filters/MyWebFilter.java
@@ -7,10 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.Set;
 
 @WebFilter(filterName = "MyWebFilter")
 public class MyWebFilter implements Filter {
+
+    private Set<String> prefixIignores = new HashSet<String>();
+
     public void destroy() {
+        prefixIignores = null;
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
@@ -24,7 +30,7 @@ public class MyWebFilter implements Filter {
         //判断用户登录
         HttpSession session = ((HttpServletRequest) req).getSession();
         if(null==session.getAttribute("username")) {
-            if(((HttpServletRequest) req).getRequestURI().contains("login")) {
+            if(_canIgnore((HttpServletRequest) req)) {
                 //放行
                 chain.doFilter(req, resp);
             } else {
@@ -37,7 +43,20 @@ public class MyWebFilter implements Filter {
     }
 
     public void init(FilterConfig config) throws ServletException {
-
+        String cp = config.getServletContext().getContextPath();
+        String ignoresParam = config.getInitParameter("ignores");
+        String[] ignoreArray = ignoresParam.split(",");
+        for (String s : ignoreArray) {
+            prefixIignores.add(cp + s);
+        }
     }
-
+    private boolean _canIgnore(HttpServletRequest request) {
+        String url = request.getRequestURI();
+        for (String ignore : prefixIignores) {
+            if (url.startsWith(ignore)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
